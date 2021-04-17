2fb17c1d2e064d561d1d3b1aa0339557135f0b4a
hua <4512454@qq.com>
2021-04-17 17:28:16 +0800

    加了一个joinTst方法

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -10,13 +10,18 @@ class Index extends Common
 
     public function fenZhi()
     {
-        return 'Git的第一次新建分支Transition_first测试987654321123456';
+        return 'Git的第一次新建分支Transition_first测试987654321123456qwerty';
 
     }
 
     public function join()
     {
         return '我是WUHUA123456';
+
+    }
+    public function joinTest()
+    {
+        return '我是WUHUA123456oinTest';
 
     }
 
