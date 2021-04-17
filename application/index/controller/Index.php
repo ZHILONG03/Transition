7f587e029efd398ebc3405a9938692fdffa1412f
hua <4512454@qq.com>
2021-04-17 17:43:48 +0800

    AAAAA

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -10,7 +10,7 @@ class Index extends Common
 
     public function fenZhi()
     {
-        return 'Git的第一次新建分支Transition_first测试987654321123456qwerty';
+        return 'AAAAAAGit的第一次新建分支Transition_first测试987654321123456qwerty';
 
     }
 
